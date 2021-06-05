566ebe6bbfa52f6e6179807fa624700ae9caf391
Maka998 <correct-marijavucetic@uns.ac.rs>
2021-06-05 23:44:49 +0200

    test repairs

--- src/test/java/rs/ac/uns/ftn/informatika/jpa/integration/PatientServiceTest.java
+++ src/test/java/rs/ac/uns/ftn/informatika/jpa/integration/PatientServiceTest.java
@@ -1,16 +1,4 @@
 package rs.ac.uns.ftn.informatika.jpa.integration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import rs.ac.uns.ftn.informatika.jpa.util.TestUtil;
-import rs.ac.uns.ftn.informatika.jpa.util.VacationInterval;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,17 +11,27 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.web.context.WebApplicationContext;
 @AutoConfigureMockMvc(addFilters = false)
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class UserServiceTest {
-    private static final String URL_PREFIX = "/api";
+public class PatientServiceTest {
+    private static final String URL_PREFIX = "/patient";
 
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
             private MockMvc mockMvc;
-    
+
             @Autowired
             private WebApplicationContext webApplicationContext;
             @Before
@@ -41,13 +39,14 @@ public class UserServiceTest {
                 this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .build();
             }
-    
+
             @Test
-            @WithMockUser(roles = "ADMIN")
-            public void isUsernameValid() throws Exception {
-                String username="balsa";
-                mockMvc.perform(get(URL_PREFIX + "/isUsernameValid/"+username).contentType(contentType))
+            public void saveDrugAll() throws Exception {
+                String drugAllergies="penicilin,cefaleksin";
+                
+              
+              
+                mockMvc.perform(get(URL_PREFIX + "/savePatientDrugAllergies/" +drugAllergies ).contentType(contentType))
                 .andExpect(status().isOk());
             }
-    }
-    
+}
